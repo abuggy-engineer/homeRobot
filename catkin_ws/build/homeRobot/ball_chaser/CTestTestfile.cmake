8629b8a776b1be33b2580c82d922535c545a04a1
abuggy-engineer <aodhanbuggy@gmail.com>
2023-05-30 18:03:02 +0100

    Project 2 submission
    Revert back to what absolutely worked

--- catkin_ws/build/homeRobot/ball_chaser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/catkin_ws/src/homeRobot/ball_chaser
-# Build directory: /home/workspace/catkin_ws/build/homeRobot/ball_chaser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
